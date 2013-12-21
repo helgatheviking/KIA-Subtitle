9897b102f7cdc5b2444a6226b9baffd80838853f
Kathy <helgatheviking@gmail.com>
2013-12-21 14:08:12 -0500

    remove unneeded JS, replace with "tab" from title to subtitle

--- scripts/subtitle.js
+++ scripts/subtitle.js
@@ -1,52 +1,26 @@
-( function( $ ) {
+;( function( $ ) {
 
 	/*
 	 * SINGLE POST SCREEN
 	 * @since 1.0
 	 */
-	 //@todo: not needed b/c wp3.5 has hook in right place - remove in next version
+	// move to just after the title
 	$( '#the_subtitle' ).insertAfter( '#title' );
 
-	//smart empty:
-	$( '#titlewrap' ).on( 'focus', '#the_subtitle', function(){
-		if( $( this ).val() == KIA_Subtitle.subtitle){
-			$( this ).val( '' ).removeClass( 'prompt' );
+	// props to Giuseppe Mazzapica for this tabbing script
+	$(document).on( 'keydown', '#title, #the_subtitle', function( e ) {
+		var keyCode = e.keyCode || e.which;
+		if ( 9 == keyCode){
+			e.preventDefault();
+			var target = $(this).attr('id') == 'title' ? '#the_subtitle' : 'textarea#content';
+			if ( (target === '#the_subtitle') || $('#wp-content-wrap').hasClass('html-active') ) {
+				$(target).focus();
+			} else {
+				tinymce.execCommand('mceFocus',false,'content');
+			}
 		}
-
 	});
 
-	$('#titlewrap').on('blur', '#the_subtitle', function(){
-		if( $( this ).val() == '' ){
-			$( this ).val( KIA_Subtitle.subtitle ).addClass( 'prompt' );
-		}
-
-	});
 
 })( jQuery );
-
-( function( $ ) {
-
-	/*
-	 * EDIT SCREEN
-	 * @since 1.1
-	 */
-
-	$( '#the-list' ).on( 'click', '.editinline', function(){
-
-		// revert Quick Edit menu so that it refreshes properly
-		inlineEditPost.revert();
-
-		tag_id = $( this ).parents( 'tr' ).attr( 'id' );
-		posttitlelabel = $( ':input[name="post_title"]', '.inline-edit-row' ).parents( 'label' );
-		subtitle = $( 'div.kia-subtitle-value', '#' + tag_id ).text();
-
-		//move the subtitle input and set its value from the hidden field
-		$( 'input.kia-subtitle-input', '.inline-edit-row' ).val( subtitle ).parents( 'label' ).insertAfter( posttitlelabel );
-
-	});
-
-})( jQuery );
-
-
-
 
